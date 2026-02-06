e3d883315a27e7549b2a6885ca576dfd0ab25ee4
aref abolhasani <arefabolhasani21@gmail.com>
2026-02-07 02:46:49 +0330

    remove pygame message

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import os
+os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "hide"
 import pygame
 
 def main():
     try:
         pygame.mixer.init()
     except pygame.error() as e:
-        print("failed to intialization audio!")
+        print("failed to intialization audio!", e)
 
 if __name__ == "__main__":
     main()
